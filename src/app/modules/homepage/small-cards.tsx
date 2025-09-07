82e2c6580088b5374a3c4b83d80f6c82c322efa2
Sereitepy OR <sereitepy.or@allweb.com.kh>
2025-09-07 16:08:40 +0700

    Dynamic Route
    When user clicks card, the URL will push to the detailed page along with the card's ID

--- src/app/modules/homepage/small-cards.tsx
+++ src/app/modules/homepage/small-cards.tsx
@@ -1,5 +1,6 @@
 import { SearchX } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useState } from 'react'
 
 interface Articles {
@@ -22,31 +23,33 @@ export function SmallCards({ data }: NewsProp) {
         <div className='grid 2xl:grid-cols-3 gap-8 sm:grid-cols-2 grid-cols-1'>
           {data.map(item => {
             return (
-              <div key={item.id}>
-                <div className='w-full flex sm:gap-4 gap-2 rounded-xl overflow-hidden hover:shadow-lg dark:shadow-md dark:hover:shadow-gray-700/80 dark:shadow-gray-800/50 shadow-sm sm:dark:shadow-inner-background sm:shadow-none transition-all duration-200 cursor-pointer'>
-                  <Image
-                    src={
-                      item?.image ??
-                      'https://app.requestly.io/delay/5000/https://heroui.com/images/hero-card-complete.jpeg'
-                    }
-                    alt={item.title}
-                    width={100}
-                    height={100}
-                    className='object-cover lg:w-30 w-25 h-auto'
-                  />
-                  <div className='p-2 flex flex-col gap-2 line-clamp-3 py-2'>
-                    <p className='lg:text-[10px] text-[9px] text-foreground'>
-                      {item.date}
-                    </p>
-                    <h2 className='font-semibold line-clamp-2 lg:text-base text-sm'>
-                      {item.title}
-                    </h2>
-                    <p className='lg:text-sm text-xs line-clamp-2'>
-                      {item.description}
-                    </p>
+              <Link href={`/articles/${item.id}`}>
+                <div key={item.id}>
+                  <div className='w-full flex sm:gap-4 gap-2 rounded-xl overflow-hidden hover:shadow-lg dark:shadow-md dark:hover:shadow-gray-700/80 dark:shadow-gray-800/50 shadow-sm sm:dark:shadow-inner-background sm:shadow-none transition-all duration-200 cursor-pointer'>
+                    <Image
+                      src={
+                        item?.image ??
+                        'https://app.requestly.io/delay/5000/https://heroui.com/images/hero-card-complete.jpeg'
+                      }
+                      alt={item.title}
+                      width={100}
+                      height={100}
+                      className='object-cover lg:w-30 w-25 h-auto'
+                    />
+                    <div className='p-2 flex flex-col gap-2 line-clamp-3 py-2'>
+                      <p className='lg:text-[10px] text-[9px] text-foreground'>
+                        {item.date}
+                      </p>
+                      <h2 className='font-semibold line-clamp-2 lg:text-base text-sm'>
+                        {item.title}
+                      </h2>
+                      <p className='lg:text-sm text-xs line-clamp-2'>
+                        {item.description}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             )
           })}
         </div>
