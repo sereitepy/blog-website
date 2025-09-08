dc07ad360318e8aa43571f013b84a67989ad3182
Sereitepy OR <sereitepy.or@allweb.com.kh>
2025-09-08 18:15:56 +0700

    Detailed Article Page that dynamically display the full articles using the articleID that user has selected
    Although the Detailed Article page is displayed according to the selected ID from the URL Param, the data used is Mock Data. I'm still unsure when to switch to backend to use real data. Or how to structure the data for the entire project.

--- src/app/[locale]/articles/[id]/page.tsx
+++ src/app/[locale]/articles/[id]/page.tsx
@@ -1,18 +1,123 @@
 'use client'
-import { use } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useParams } from 'next/navigation'
+import { ArrowLeftIcon } from '@phosphor-icons/react/ssr'
 
-export default function BlogPostPage({
-  params,
-}: {
-  params: Promise<{ slug: string }>
-}) {
-  const { slug } = use(params)
-  console.log('slug', slug)
+const news = [
+  {
+    id: 1,
+    title:
+      'Government refutes fabricated story of Sam Rainsy, who accused Samdech Techo Hun Sen of buying shares in Thai energy company',
+    description:
+      'On September 2, 2025, the Royal Government Spokesperson issued a statement denying false information posted on the Facebook account of Sam Rainsy, falsely alleging that Samdech Techo Hun Sen, President of the Senate, was involved in the purchase of 24% of shares in a major Thai energy company called Bangchak for billions of baht through a South African middleman, but that the Thai Social Security Fund refused to sell it.',
+    date: '2025-09-02',
+    image:
+      'https://oslofreedomforum.com/wp-content/uploads/2024/02/Speaker-Sam-Rainsy-V1.png',
+  },
+  {
+    id: 2,
+    title: 'Cambodia and Laos to strengthen cooperation in border trade',
+    description:
+      'On September 1, 2025, Cambodian Prime Minister Samdech Techo Hun Sen and Lao Prime Minister Sonexay Siphandone held a bilateral meeting on the sidelines of the 78th UN General Assembly in New York, USA. They discussed enhancing cooperation in border trade and agreed to expedite the construction of the Vientiane-Vang Tao railway project to boost economic growth and connectivity between the two countries.',
+    date: '2025-09-01',
+    image:
+      'https://www.aljazeera.com/wp-content/uploads/2017/08/262a18fdbc3145a69c1a88ccbc3bdea5_18.jpeg',
+  },
+  {
+    id: 3,
+    title:
+      'Cambodia urges Thailand to release 18 captured soldiers amid ongoing tensions',
+    description:
+      'The Ministry of National Defence confirmed that the 18 Cambodian soldiers captured by Thai military forces after the ceasefire have not been released. Spokesperson Lt Gen Maly Socheata called on Thailand to promptly return the soldiers to Cambodia.',
+    date: '2025-09-01',
+    image:
+      'https://s.france24.com/media/display/014b9f50-694a-11f0-9659-005056a97e36/w:1280/p:16x9/000-67NV49F-1.jpg',
+  },
+  {
+    id: 4,
+    title: 'Cambodia and Thailand agree to a ceasefire after border clashes',
+    description:
+      'On August 31, 2025, Cambodia and Thailand agreed to a ceasefire following border clashes that resulted in casualties on both sides. The agreement was reached after high-level talks between the two countries, with both sides committing to de-escalate tensions and work towards a peaceful resolution of their border disputes.',
+    date: '2025-08-31',
+    image:
+      'https://i0.wp.com/fulcrum.sg/wp-content/uploads/AFP__20250820__69Z83T3__v1__HighRes__CambodiaThailandBorderConflict-e1756139812704.jpg?fit=1248%2C562&ssl=1',
+  },
+  {
+    id: 5,
+    title: 'Cambodia, China pledge to deepen trade, investment ties',
+    description:
+      'Both leaders agree on expanding bilateral trade and investment under the Cambodia-China Free Trade Agreement (CCFTA), citing the need to diversify supply chains and attract high-value industries.',
+    date: '2025-09-2',
+    image:
+      'https://www.idcpc.gov.cn/english2023/ttxw_5749/202302/W020230728406753223084.png',
+  },
+  {
+    id: 'brushsggyg',
+    title: 'Videography Basics',
+    description: 'Learn the fundamentals of videography.',
+    date: '2025-09-01',
+  },
+  {
+    id: 'xvcthryhy',
+    title: 'Advanced Editing Techniques',
+    date: '2025-09-01',
+    description: 'Take your editing skills to the next level.',
+  },
+  {
+    id: 'asdzxcyty',
+    title: 'Photography Composition',
+    date: '2025-09-01',
+    description: 'Master the art of composition in photography.',
+  },
+  {
+    id: 'qweasdzxc',
+    title: 'Lighting for Photos',
+    date: '2025-09-01',
+    description: 'Understand how to use lighting effectively.',
+  },
+  {
+    id: 'plmoknijn',
+    title: 'Camera Settings Explained',
+    date: '2025-09-01',
+    description: 'Get to know your camera settings inside out.',
+  },
+]
+
+export default function BlogPostPage() {
+  const params = useParams<{ id: string }>()
+  const findArticle = news.find(item => item.id.toString() === params.id)
 
   return (
-    <div>
-      <h1>Ayo</h1>
-      <p className='text-xl'>Slug: {slug}</p>
+    <div className='max-w-5xl mx-auto '>
+      {findArticle && (
+        <div className='flex sm:items-center justify-center flex-col gap-4 w-full'>
+          <Link href={'/'} className='self-start'>
+            <div className='py-1 flex gap-2 items-center p-3 hover:bg-gray-100 border-1 dark:hover:bg-gray-800 rounded-full w-fit cursor-pointer '>
+              <ArrowLeftIcon size={16} />
+              <p className='text-sm'>Go Back</p>
+            </div>
+          </Link>
+          <h1 className='text-2xl font-bold sm:text-center'>
+            {findArticle.title}
+          </h1>
+          <p className='text-sm'>Published Date: {findArticle.date}</p>
+          <Image
+            src={
+              findArticle.image ??
+              'https://app.requestly.io/delay/5000/https://heroui.com/images/hero-card-complete.jpeg'
+            }
+            alt={findArticle.title}
+            width={500}
+            height={300}
+            className='rounded-lg max-h-100 w-auto object-contain'
+          />
+          <div className='flex flex-col gap-2'>
+            <h2 className='font-semibold text-xl'>Description</h2>
+            <h3>{findArticle.description}</h3>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
