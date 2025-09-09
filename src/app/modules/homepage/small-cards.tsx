0ce1b2de1fff4440321c58becd59a799b51467aa
Sereitepy OR <sereitepy.or@allweb.com.kh>
2025-09-09 18:26:58 +0700

    Update MockData and loading image feature
    The loading image is the loading icon rotating while waiting for image

--- src/app/modules/homepage/small-cards.tsx
+++ src/app/modules/homepage/small-cards.tsx
@@ -1,7 +1,9 @@
+import { SpinnerGapIcon } from '@phosphor-icons/react/ssr'
 import { SearchX } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
+import { motion } from 'motion/react'
 
 interface Articles {
   id: number | string
@@ -26,16 +28,34 @@ export function SmallCards({ data }: NewsProp) {
               <div key={item.id}>
                 <Link href={`/articles/${item.id}`}>
                   <div className='w-full flex sm:gap-4 gap-2 rounded-xl overflow-hidden hover:shadow-lg dark:shadow-md dark:hover:shadow-gray-700/80 dark:shadow-gray-800/50 shadow-sm sm:dark:shadow-inner-background sm:shadow-none transition-all duration-200 cursor-pointer'>
-                    <Image
-                      src={
-                        item?.image ??
-                        'https://app.requestly.io/delay/5000/https://heroui.com/images/hero-card-complete.jpeg'
-                      }
-                      alt={item.title}
-                      width={100}
-                      height={100}
-                      className='object-cover lg:w-30 w-25 h-auto'
-                    />
+                    {item?.image ? (
+                      <Image
+                        src={item.image}
+                        alt={item.title}
+                        width={100}
+                        height={100}
+                        className='object-cover lg:w-30 w-25 h-auto'
+                      />
+                    ) : (
+                      <div className='flex items-center justify-center lg:w-30 w-100 h-auto animate-pulse dark:bg-gray-500 bg-gray-300'>
+                        <div className='w-100 flex items-center justify-center'>
+                          <motion.div
+                            className='spinner'
+                            animate={{ rotate: 360 }}
+                            transition={{
+                              duration: 1.5,
+                              repeat: Infinity,
+                              ease: 'linear',
+                            }}
+                          >
+                            <SpinnerGapIcon
+                              size={20}
+                              className='text-gray-500 dark:text-black'
+                            />
+                          </motion.div>
+                        </div>
+                      </div>
+                    )}
                     <div className='p-2 flex flex-col gap-2 line-clamp-3 py-2'>
                       <p className='lg:text-[10px] text-[9px] text-foreground'>
                         {item.date}
