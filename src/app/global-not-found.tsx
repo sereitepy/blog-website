bd8076be5527c1984aaf6eb13a6e1f42b25b3299
sereitepy <106307116+sereitepy@users.noreply.github.com>
2025-10-24 18:28:56 +0700

    Translate Header and Footer, update font-size to be a bit bigger

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -21,12 +21,12 @@ const geistMono = Geist_Mono({
 function Message() {
   const t = useTranslations("notFound")
   return (
-    <div>
+    <div className='transition delay-200 ease-initial'>
       <div className='flex flex-col justify-center items-center gap-5 max-[770px]:min-h-100 my-10'>
-        <h1 className='text-xl font-bold lg:text-2xl'>{t("title")}</h1>
-        <p className='text-sm md:text-md lg:text-lg'>{t("body")}</p>
+        <h1 className='text-xl font-bold md:text-2xl'>{t("title")}</h1>
+        <p className='text-md md:text-lg text-center'>{t("body")}</p>
         <Button>
-          <Link href='/' className='text-md lg:text-lg font-bold'>{t("buttonMessage")}</Link>
+          <Link href='/' className='text-md md:text-lg font-bold'>{t("buttonMessage")}</Link>
         </Button>
       </div>
     </div>
@@ -50,7 +50,7 @@ export default async function GlobalNotFound() {
               <div className='sticky -top-1 bg-inner-background border-b-1'>
                 <Header />
               </div>
-              <div className='flex items-center justify-center sm:px-20 py-10 bg-inner-background flex-grow'>
+              <div className='flex items-center justify-center px-5 sm:px-20 py-10 bg-inner-background flex-grow'>
                 <Message />
               </div>
               <footer className='bg-inner-background border-t-1'>
--- src/app/modules/footer/index.tsx
+++ src/app/modules/footer/index.tsx
@@ -3,42 +3,43 @@ import {
   LinkedinLogoIcon,
   XLogoIcon,
 } from '@phosphor-icons/react/ssr'
+import { useTranslations } from 'next-intl'
 import Image from 'next/image'
-
-const footerLinks = [
-  {
-    id: 'about',
-    title: 'About',
-    details: 'Details',
-    software: 'Software Experience',
-    articles: 'Related Articles',
-  },
-  {
-    id: 'resources',
-    title: 'Resources',
-    details: 'Camera',
-    software: 'Editing Software',
-    articles: 'Articles',
-  },
-  {
-    id: 'support',
-    title: 'Support',
-    details: 'Contact Us',
-    software: 'FAQs',
-    articles: 'Help Center',
-  },
-]
 
 export const Footer = () => {
   const date = new Date()
   let year = date.getFullYear()
+  const t = useTranslations('footer')
+  const footerLinks = [
+    {
+      id: 'about',
+      title: `${t('about')}`,
+      details: `${t('randomLink')}`,
+      software: `${t('randomLink')}`,
+      articles: `${t('randomLink')}`,
+    },
+    {
+      id: 'resources',
+      title: `${t('resources')}`,
+      details: `${t('randomLink')}`,
+      software: `${t('randomLink')}`,
+      articles: `${t('randomLink')}`,
+    },
+    {
+      id: 'support',
+      title: `${t('support')}`,
+      details: `${t('randomLink')}`,
+      software: `${t('randomLink')}`,
+      articles: `${t('randomLink')}`,
+    },
+  ]
   return (
     <footer className='bg-inner-background max-w-[2050px] mx-auto sm:py-15 sm:px-20 px-10 py-8'>
       <div className='flex flex-col gap-8'>
         {/* upper section */}
-        <div className='flex sm:flex-row flex-col gap-8 sm:gap-0 justify-between items-center'>
+        <div className='flex sm:flex-row flex-col gap-6 sm:gap-2 justify-between min-[350px]:items-center'>
           {/* left section */}
-          <div className='flex flex-col gap-4 sm:w-[45%] xl:w-[60%]'>
+          <div className='flex flex-col gap-6 xs:w-[20%] sm:w-[40%] md:w-[60%] xl:w-[75%]'>
             <Image
               src='/logo.png'
               alt="Len's Pro Media Logo"
@@ -46,52 +47,50 @@ export const Footer = () => {
               height={55}
               className='h-auto w-15'
             />
-            <p className='text-xs sm:w-[80%] lg:text-sm'>
-              Lens Pro Media, led by media expert Kimsour, offers photography
-              and videography education to empower Cambodia's next generation
-              with media knowledge.
-            </p>
+            <p className='text-md sm:w-[80%] lg:text-lg'>{t('description')}</p>
             <div className='flex gap-3 cursor-pointer'>
-              <XLogoIcon size={18} />
-              <InstagramLogoIcon size={18} />
-              <LinkedinLogoIcon size={18} />
+              <XLogoIcon size={20} />
+              <InstagramLogoIcon size={20} />
+              <LinkedinLogoIcon size={20} />
             </div>
           </div>
 
           {/* right side */}
-            <div className='grid grid-cols-3 sm:gap-4 sm:w-[55%] xl:w-[40%]'>
-              {footerLinks.map(item => {
-                return (
-                  <div
-                    key={item.id}
-                    className='flex flex-col sm:gap-2 gap-1 cursor-pointer w-fit ml-auto'
-                  >
-                    <h3 className='font-semibold sm:text-base text-sm'>
-                      {item.title}
-                    </h3>
-                    <p className='sm:text-xs text-[11px] hover:underline hover:underline-offset-4 lg:text-sm'>
-                      {item.details}
-                    </p>
-                    <p className='sm:text-xs text-[11px] hover:underline hover:underline-offset-4 lg:text-sm'>
-                      {item.software}
-                    </p>
-                    <p className='sm:text-xs text-[11px] hover:underline hover:underline-offset-4 lg:text-sm'>
-                      {item.articles}
-                    </p>
-                  </div>
-                )
-              })}
-            </div>
+          <div className='grid grid-cols-3 max-[350px]:grid-cols-2 gap-12 max-[450px]:gap-5 sm:gap-4 sm:w-[60%] xl:w-[50%]'>
+            {footerLinks.map(item => {
+              return (
+                <div
+                  key={item.id}
+                  className='flex flex-col sm:gap-4 gap-2 cursor-pointer sm:w-fit sm:ml-auto w-full'
+                >
+                  <h3 className='font-semibold sm:text-lg text-md'>
+                    {item.title}
+                  </h3>
+                  <p className='sm:text-sm max-[450px]:text-sm text-[16px] hover:underline hover:underline-offset-4 lg:text-[16.5px]'>
+                    {item.details}
+                  </p>
+                  <p className='sm:text-sm max-[450px]:text-sm text-[16px] hover:underline hover:underline-offset-4 lg:text-[16.5px]'>
+                    {item.software}
+                  </p>
+                  <p className='sm:text-sm max-[450px]:text-sm text-[16px] hover:underline hover:underline-offset-4 lg:text-[16.5px]'>
+                    {item.articles}
+                  </p>
+                </div>
+              )
+            })}
+          </div>
         </div>
         {/* lower section */}
-        <div className='flex flex-col gap-3 sm:flex-row sm:justify-between sm:items-center text-xs'>
-          <div>&copy;&nbsp;{year} Lens Pro Media All rights reserved</div>
-          <div className='flex gap-4 items-center sm:text-xs/3 text-[10px] cursor-pointer'>
+        <div className='flex flex-col gap-3 sm:flex-row sm:justify-between sm:items-center text-sm'>
+          <div>
+            &copy;{year} {t('copyRight')}
+          </div>
+          <div className='flex gap-4 min-[350px]:items-center sm:text-md text-sm cursor-pointer max-[350px]:flex-col'>
             <p className='hover:underline hover:underline-offset-4'>
-              Privacy Policy
+              {t('privacyPolicy')}
             </p>
             <p className='hover:underline hover:underline-offset-4'>
-              Term's of Service
+              {t('termsOfService')}
             </p>
           </div>
         </div>
